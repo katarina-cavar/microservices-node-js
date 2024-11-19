02c30b2e707dc07f27d73ca62569d756a95cb247
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2024-11-19 15:10:46 +0100

    Lesson 22: Fetching and rendering posts

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 const App = () => {
 	return <div className='container'>
 		Blog app
 		<h1>CreatePost</h1>
 		<PostCreate />
+		<hr />
+		<h1>Posts</h1>
+		<PostList />
 	</div>;
 };
 
