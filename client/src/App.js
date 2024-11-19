0351d346eb595d8c5a80d6a5181704f59eeda0a0
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2024-11-19 14:12:29 +0100

    Lesson 20: Building post submission

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PostCreate from './PostCreate';
 
 const App = () => {
-	return <div>Blog app</div>;
+	return <div className='container'>
+		Blog app
+		<h1>CreatePost</h1>
+		<PostCreate />
+	</div>;
 };
 
 export default App;
