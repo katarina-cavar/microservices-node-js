80917db505bd008dcf656f452d3d97bb3e2f69c6
Katarina Cavar <katarina.cavar@cloudsensesolutions.com>
2024-11-19 15:42:45 +0100

    Lesson 24: Displaying comments

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const CommentList = (props) => {
-	return <div>CommentList</div>
+const CommentList = ({ postId }) => {
+	const [comments, setComments] = useState([]);
+
+	const fetchData = async () => {
+		const res = await axios.get(
+			`http://localhost:4001/posts/${postId}/comments`
+		);
+
+		setComments(res.data);
+	};
+
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const renderedComments = comments.map((comment) => {
+		return <li key={comment.id}>{comment.content}</li>
+	});
+
+	return <ul>{renderedComments}</ul>;
 };
 
 export default CommentList;
